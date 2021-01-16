7f23a6293a5215ba90c8bff9649b6d7e95e9cd9e
Ryan-Sin <ryansin937@gmail.com>
2021-01-16 13:20:09 +0900

    feat : cors 추가

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 //express 모듈 불러오기
 const express = require("express");
 
+//cors 설정
+const cors = require('cors');
+
 //express 사용
 const app = express();
 
 //Express 4.16.0버전 부터 body-parser의 일부 기능이 익스프레스에 내장 body-parser 연결 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
+app.use(cors()); // Test 버전으로 설정(이 설정은 모든 도메인을 허용한다는 뜻 실 서비스에 대포시 주의 :> )
 
 //임시 데이터
 const users = [
